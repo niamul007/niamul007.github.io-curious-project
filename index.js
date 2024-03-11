6ac765593d012437dcf91fa6b410300caa236e54
niamul007 <am3994566@gmail.com>
2024-03-11 16:53:57 +0600

    add some new things

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 let sidebar =document.querySelector(".sidebar");
+let body = document.getElementsByTagName("body");
 
 function showSidebar(){
     sidebar.style.display="block"
 }
+let bar = document.querySelector(".nav-link");
+bar.addEventListener("click",()=>{
+    body.style.filter="blur(5px);"
+
+})
 
 
 function removeBar(){

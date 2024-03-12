9f0075dcc0d1d18dc24b2e4d6275cf030ea589dd
niamul007 <am3994566@gmail.com>
2024-03-12 15:34:56 +0600

    add some new pages

--- index.js
+++ index.js
@@ -3,16 +3,10 @@ let body = document.getElementsByTagName("body");
 
 function showSidebar(){
     sidebar.style.display="block"
+
 }
-let bar = document.querySelector(".nav-link");
-bar.addEventListener("click",()=>{
-    body.style.filter="blur(5px);"
-
-})
-
 
 function removeBar(){
     sidebar.style.display="none"
-
 }
 
